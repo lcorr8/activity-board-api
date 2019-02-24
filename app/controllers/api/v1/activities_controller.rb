16d0d8ad37652b22b3fccd144f77ad3c4c03d343
Laura Correa <laura.correa@digitalunit.berlin>
2019-02-24 11:45:23 +0100

    handle create action

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -4,5 +4,19 @@ module Api::V1
         @activities = Activity.all
         render json: @activities
       end
+
+      def create 
+        puts "activity params"
+        puts activity_params
+        # raise activity_params.inspect 
+        @activity = Activity.create(activity_params)
+        render json: @activity
+      end
+      
+      private
+      
+        def activity_params
+          params.require(:activity).permit(:title, :neighborhood, :time, :link, :notes, :activity_type)
+        end
     end
   end
